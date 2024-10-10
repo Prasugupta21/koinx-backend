45dd6fa3e79fccb4843cf708be22cd1a94861599
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-10-10 19:31:25 +0530

    task 1 completed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-
+const cron=require('node-cron');
+const fetchAndStoreCryptoJob=require('./controllers/fetchAndStoreCryptoJob');
 const app = express();
 
 const fetchAndStore = require("./routes/fetchAndStore");
@@ -9,6 +10,12 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", fetchAndStore);
+
+
+cron.schedule('0 */2 * * *', async () => {
+    console.log("Running background job to fetch and store crypto data...");
+    await fetchAndStoreCryptoJob();
+  });
 
 app.listen(3000, () => {
   console.log("server started");
