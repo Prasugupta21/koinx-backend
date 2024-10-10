248d738dfb94c86c8aeec62ab8b411747d23655e
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-10-10 11:20:14 +0530

    server start

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const app=express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+app.listen(3000,(req,res)=>{
+    console.log('server started');
